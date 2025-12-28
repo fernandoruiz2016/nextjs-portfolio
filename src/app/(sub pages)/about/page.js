5eeeeb69ea7b43ecc28c657bfa9dc128fabe15c2
Fernando Ruiz <fernandoruiz696@gmail.com>
2025-12-28 18:43:18 -0500

    Responsive about

--- src/app/(sub pages)/about/page.js
+++ src/app/(sub pages)/about/page.js
@@ -18,7 +18,7 @@ export default function Home() {
                 </RenderModel>
             </div>
 
-            <div className="relative w-full h-screen flex flex-col items-center justify-center">
+            <div className="relative w-full h-screen flex flex-col items-center justify-center mt-32 xs:mt-20">
                 <div className="absolute flex flex-col items-center text-center top-1/2 sm:top-[60%] left-1/2 -translate-y-1/2 -translate-x-1/2">
                     <h1 className="w-[90vw] overflow-hidden font-bold text-6xl mt-4 xs:text-4xl sm:text-5xl lg:text-6xl text-accent">Fernando Alonso Ruiz Yamamoto</h1>
                     <p className="font-light text-foreground text-ls">Meet the wizard behind this portfolio</p>
